2b23af3592dca7ba018026b79a06c49545d3df93
lier3322 <baoguix@gmail.com>
2020-08-24 19:30:06 +0900

    fix

--- app/assets/javascripts/modules/user.js
+++ app/assets/javascripts/modules/user.js
@@ -18,6 +18,18 @@ $(function() {
     $("#UserSearchResult").append(html);
   }
 
+  function addMember(name, id) {
+    let html = `
+                <div class="ChatMember">
+                  <p class="ChatMember__name">${name}</p>
+                  <input name="group[user_ids][]" type="hidden" value="${id}" />
+                  <div class="ChatMember__remove ChatMember__button">削除</div>
+                </div>
+                `;
+    $(".ChatMembers").append(html);
+  }
+
+
   $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
     $.ajax({
